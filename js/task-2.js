ae15117d5b616000710e5a957cf8022d9332d87c
YehorK88 <141238245+YehorK88@users.noreply.github.com>
2023-12-22 20:58:33 +0200

    correction

--- js/task-2.js
+++ js/task-2.js
@@ -2,7 +2,7 @@
 
 function makeArray(firstArray, secondArray, maxLength) {
   const newArray = firstArray.concat(secondArray);
-  if (newArray < maxLength) {
+  if (newArray.length > maxLength) {
     return newArray.slice(0, maxLength);
   } else {
     return newArray;
